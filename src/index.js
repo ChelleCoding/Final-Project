7d2ac8188295c67faffe5ec289577fd04d16c269
chelleann40 <pz8402@gmail.com>
2024-05-19 23:18:00 -0400

    Finished adding forecast data from API.

--- src/index.js
+++ src/index.js
@@ -77,6 +77,13 @@ function formatDate(date) {
   return fullDate;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
    let apiKey = "e0e116f3440f7e76eb34tfo465e544a7";
 let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -94,11 +101,13 @@ console.log(response);
 
 
 
-  response.data.daily.forEach(function(day) {
+  response.data.daily.forEach(function(day, index){
+    console.log(index);
+    if (index >0 && index < 6)
     forecastHtml = 
     forecastHtml + `
     <div class="weather-forecast-day">
-    <div class="weather-forecast-date">Tue</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
     <div>
     <img src="${day.condition.icon_url}"class="weather-forecast-icon"/></div>
     <div class="weather-forecast-temperatures">
